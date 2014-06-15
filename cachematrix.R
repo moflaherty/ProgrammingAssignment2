7f87cc0928852cc5c0a74cccb3c009faf2948974
moflaherty <moflaherty@eInvasion.com>
2014-06-15 16:42:01 -0400

    Added a comment header so I can test the submission process to the repository.

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,10 @@
+# =============================================================
+# Author:  	   Michael O'Flaherty (michael@oflaherty.com)
+# Create date: 6/15/2014
+# Description: Created the fork from 
+#              https://github.com/rdpeng/ProgrammingAssignment2
+# =============================================================
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
@@ -6,7 +13,6 @@
 makeCacheMatrix <- function(x = matrix()) {
 
 }
-
 
 ## Write a short comment describing this function
 
